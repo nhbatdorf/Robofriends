0c06e69118eefdcacb31ad8412144d6f857faf26
Nick B <nhbatdorf@gmail.com>
2020-02-17 22:35:28 -0500

    Updates

---
+++ precache-manifest.096a5ef05676b7e9dfa99e6ccfa9e83b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91beeaa2e1597cc928c8465264e67ba5",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "88bae5fcb27fcad9d35b",
+    "url": "/Robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "13e025477492ba1997e7",
+    "url": "/Robofriends/static/css/main.29dd0eb9.chunk.css"
+  },
+  {
+    "revision": "88bae5fcb27fcad9d35b",
+    "url": "/Robofriends/static/js/2.2475d6f8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Robofriends/static/js/2.2475d6f8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "13e025477492ba1997e7",
+    "url": "/Robofriends/static/js/main.fd7c348d.chunk.js"
+  },
+  {
+    "revision": "9eff52c4d1627d87e058",
+    "url": "/Robofriends/static/js/runtime-main.b221b423.js"
+  }
+]);
